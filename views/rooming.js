6564f13bd7fe49cefaf81e9b7f9a2f7055aa4d27
KartikJoshiUK <kartikjoshiuk@gmail.com>
2024-02-24 21:34:23 +0530

    connection done

--- views/rooming.js
+++ views/rooming.js
@@ -1,1 +1,11 @@
-console.log("hii")
+var text = document.getElementById("url");
+function copyToClip(e) {
+  console.log(e);
+  var text = document.getElementById("url");
+  text.select();
+  document.execCommand("copy");
+  e.target.value = "Copied to clipboard";
+  setTimeout(function () {
+    e.target.value = "<%=roomId%>";
+  }, 1500);
+}
